2891f2dcf449056ca020427ba84e67aaf6885ac6
SilverFox70 <wfbrinkert@gmail.com>
2016-08-26 10:33:47 -0700

    add form to page

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.controller('MainCtrl', [
       {title: 'post 3', upvotes: 15},
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}];
+    $scope.addPost = function(){
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
 }]);
